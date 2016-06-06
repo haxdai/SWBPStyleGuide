8be01edca16349a5181e58bb45e54d771b0750ed
Hasdai Pacheco <haxdai@gmail.com>
2016-06-06 11:22:26 -0500

    Added SWBP color palette

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(document).ready(function() {
+	$(window).scroll(function(){
+	  if ($(this).scrollTop()>200) {
+	    $(".go-top").fadeIn(200);
+	  } else {
+	    $(".go-top").fadeOut(200);
+	  }
+	});
+
+	$('.go-top').click(function(event){
+	  event.preventDefault();
+	  $('html, body').animate({scrollTop:0}, 300);
+	});
+});
